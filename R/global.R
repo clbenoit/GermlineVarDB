2241d86b918b7d815bc489030c7f5e4a76872860
clbenoit <benoitclement.sand@gmail.com>
2023-12-13 15:47:20 +0100

    Add manifest handler in parameters management system

--- R/global.R
+++ R/global.R
@@ -14,8 +14,11 @@ global <- quote({
                           coveragenum = "mysetup", 
                           qualitynum = "mysetup", 
                           impact = "mysetup",
-                          trlist = "mysetup")
+                          trlist = "mysetup",
+                          manifest = "mysetup")
     DBI::dbWriteTable(con, name = "presets", value = presets, overwrite = TRUE)}
+  
+  DBI::dbExecute(conn = con, "CREATE TABLE IF NOT EXISTS manifests_list (user_id TEXT, manifests TEXT);")
   
   tictoc::toc()
   
